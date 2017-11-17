9f3478fdcd78a365de8159f9d56c8e88c7ccd8e7
ab-anand <abhinavanand1905@gmail.com>
2017-11-17 12:50:18 +0530

    scoop-app

---
+++ src/list.js
@@ -0,0 +1,30 @@
+// it contains the list data 
+
+const list= [
+	{
+		title: 'Tell me the truth without words',
+		url:'https://www.github.com',
+		author:'Kira',
+		num_comments:21,
+		points:50,
+		objectID:1
+	},
+	{
+		title: 'Watch your move',
+		url:'https://www.github.com',
+		author:'Ryuzaki',
+		num_comments:23,
+		points:56,
+		objectID:2
+	},
+	{
+		title: 'Sinigami Realm rules',
+		url:'https://www.github.com',
+		author:'Joker',
+		num_comments:11,
+		points:20,
+		objectID:3
+	}
+]
+
+export default list;
